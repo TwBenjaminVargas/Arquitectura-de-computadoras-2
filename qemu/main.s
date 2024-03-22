2f4ddfe30884d62f7752e4569c1199630c97786d
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2024-03-22 09:55:22 -0300

    Class 3 Eje 5.1

--- qemu/main.s
+++ qemu/main.s
@@ -1,28 +1,44 @@
-//      EJERCICIO 3.2
-
-
-.data
-f: .dword 1
-g: .dword 2
-h: .dword 3
-i: .dword 4
-j: .dword 5
-
-.text
-//LOAD VALUES TO CUSTOM REGISTERS
-        LDR     X0, f
-        LDR     X1, g
-        LDR     X2, h
-        LDR     X3, i
-        LDR     X4, j
-
-/*CODE LEGV8
-        ADDI X0,X0,#5 //f=f+5
-        SUB X0,X1,X0 // f=g-f = g + (-f-5)
+//              EJERCICIO 5.1
+/*Asignaciones
+x10 <-> i
+x1 <-> a
+x2 <-> result
+x0 <->memArray
 */
-//CODE ARMv8
-        ADD X0,X0,#5
-        SUB X0,X1,X0
-infloop:
-        B infloop
+
+//ARMv8 CODE:
+.data /// segmen to de d a t o s
+memArray: .dword 1, 2, 3, 4
+i: .word 0
+result: .dword 0
+a: .dword 0
+offset: .dword 0x40080000
+.text
+        LDR X0,=memArray
+        LDR X3,offset
+        ADD X0,X0,X3
+        LDR X1,a
+        LDR x2,result
+        ADD X10, XZR, XZR
+loop:   
+        LDR X1, [X0,#0]
+        ADD X2, X2, x1
+        ADD X0, X0, #8
+        ADD X10, X10, #1
+        CMP X10, #3
+        B.LT loop
+
+/* C CODE:
+        int i = 0;
+        long a=0, result=0;
+        do
+        {
+                a = *memArray;
+                result += a;
+                memArray+=1;
+                i++;
+        }
+        while(i < 3);
+*/
+infloop: b infloop
 
